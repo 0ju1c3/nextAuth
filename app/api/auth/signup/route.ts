564b82b8c0d261ed124b1c6b9881d58bcc619286
0ju1c3 <otapadia@gmail.com>
2024-11-10 21:24:56 +0530

    feat: added edge case of user already exists

--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -12,6 +12,16 @@ export async function POST(req: Request) {
     const hashedPassword = await bcrypt.hash(password, 10);
 
     try {
+        const checkUser = await prisma.user.findFirst({
+            where: {
+                username: username
+            }
+        })
+
+        if (checkUser) {
+            return NextResponse.json({ message: "User already exists" }, { status: 400 })
+        }
+
         const newUser = await prisma.user.create({
             data: {
                 username: username,
